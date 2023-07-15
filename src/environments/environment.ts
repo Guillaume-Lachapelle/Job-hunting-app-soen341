cd7c13e0e629aea7b248bbbad4bacd5846d61b92
Guillaume-Lachapelle <guillaume2.lachapelle@gmail.com>
2023-07-15 19:49:12 -0400

    Added firebase hosting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,13 @@
 export const environment = {
-    prduction:false,
-    firebase: {
-        apiKey: "AIzaSyDzhee1B3JD7RKG9YUdoAwhLl7Jt4C8iys",
-        authDomain: "squared-e7104.firebaseapp.com",
-        databaseURL: "https://squared-e7104-default-rtdb.firebaseio.com",
-        projectId: "squared-e7104",
-        storageBucket: "squared-e7104.appspot.com",
-        messagingSenderId: "899166822643",
-        appId: "1:899166822643:web:e05743825f6141a0399786",
-        measurementId: "G-SP4ENB128C"
-      }
-
+  prduction: false,
+  firebase: {
+    apiKey: 'AIzaSyBeBZevDhRPa1PpadSDM7CjTwDkXR0p1fE',
+    authDomain: 'job-hunting-app-d1b2f.firebaseapp.com',
+    databaseURL: 'https://job-hunting-app-d1b2f-default-rtdb.firebaseio.com',
+    projectId: 'job-hunting-app-d1b2f',
+    storageBucket: 'job-hunting-app-d1b2f.appspot.com',
+    messagingSenderId: '1083326547367',
+    appId: '1:1083326547367:web:219b640d757d247e07a282',
+    measurementId: 'G-VVZNNCTELH',
+  },
 };
